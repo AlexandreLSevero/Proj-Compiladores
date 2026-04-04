dff58f6edad2d9b1ccf926daebe0f7746699bffb
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2026-04-04 11:46:11 -0300

    Update symtab.h

--- include/symtab.h
+++ include/symtab.h
@@ -1,7 +1,40 @@
-typedef enum { CAT_VAR, CAT_PROC, CAT_FUNC, CAT_PARAM } SymbolCat;
-typedef enum { TYPE_INT, TYPE_VOID, TYPE_BOOL } DataType;
+#ifndef SYMTAB_H
+#define SYMTAB_H
 
-void ts_push_scope(const char *scope_name);
-void ts_pop_scope();
-void ts_insert(const char *id, SymbolCat cat, DataType type, int extra);
+#include "common.h"
+
+// Categorias de símbolos na tabela
+typedef enum {
+    SYM_VAR,
+    SYM_VETOR,
+    SYM_PROC,
+    SYM_FUNC,
+    SYM_PARAM
+} SymbolCat;
+
+// Entrada da Tabela de Símbolos
+typedef struct {
+    char id[256];
+    SymbolCat cat;
+    DataType type;
+    int extra;      // Tamanho do vetor ou Qtd de parâmetros
+    char scope[128];
+} Symbol;
+
+// Gerenciamento de Escopo
+void ts_scope_push(const char *name);
+void ts_scope_pop();
+const char* ts_scope_current();
+
+// Operações da Tabela
+bool ts_insert(const char *id, SymbolCat cat, DataType type, int extra);
 Symbol* ts_lookup(const char *id); // Busca do escopo atual para o global
+
+// Inicialização e finalização
+void ts_init();
+void ts_free();
+
+// Função para o log --symtab (percorre a tabela e exporta)
+void ts_print_log(FILE *out);
+
+#endif
