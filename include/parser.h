e5e7f87664097e6875f0b5ecc9e10d89a23f42b2
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2026-04-04 11:47:42 -0300

    Create parser.h

---
+++ include/parser.h
@@ -0,0 +1,10 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdbool.h>
+
+// Função principal que inicia a análise sintática do programa SAL.
+// Retorna true se o programa estiver sintaticamente correto.
+bool parse_program();
+
+#endif
